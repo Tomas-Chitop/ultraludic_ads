6df68ca2f21d9d845700eb74b0beb50780529550
Tomas Chitop <tom.88chitop@gmail.com>
2022-09-15 10:05:31 -0600

    Arch de mas informacion

---
+++ mas_informacion.php
@@ -0,0 +1,28 @@
+<?php include("template/header.php") ?>
+
+<div class="container">
+  <br>
+  <div class="row">
+      <div class="col-md-6">
+            
+      <div class="card mb-3">
+      <h1 class="card-header">Coca-Cola</h1>
+      <div class="card-body">
+        <h2 class="card-title">Coca-cola</h2>
+        <h4 class="card-subtitle text-muted">¡Invierte con nosotros!</h4>
+        <h5 class="card-subtitle text-muted">
+          Desde lo más básico hasta un nivel experto para formarte en el mundo del desarrollo web.
+        </h5>
+      </div>
+      <img width="350" height="420" src="https://empresas.blogthinkbig.com/wp-content/uploads/2019/11/Imagen3-245003649.jpg?w=800" alt="img-anuncio">
+      <div class="card-body">
+        <p class="card-text">+502 0000-0000</p>
+      </div>
+      <div>
+      <div class="card-footer text-muted">
+      </div>
+        
+  </div>
+</div>
+
+<?php include("template/footer.php") ?>
