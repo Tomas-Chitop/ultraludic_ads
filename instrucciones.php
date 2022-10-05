3debc7bb5d1ec5030e939814343517e05de38c36
Tomas Chitop <tom.88chitop@gmail.com>
2022-10-05 04:05:40 -0600

    para subir

--- instrucciones.php
+++ instrucciones.php
@@ -1,6 +1,6 @@
 <?php include("template/header.php") ?>
 
-<center><a href="https://www.spotify.com" target="_blank"><img onclick="coutingClicks()" style="display:inline-block;" height="350px" width="300px" src="http://localhost/ultraludic_ADS/img/1664657883_giphy.gif" alt="¡Anuncio no disponible!"></a></center>
+<center><a href="https://www.ultraludic.com" target="_blank"><img onclick="coutingClicks()" style="display:inline-block;" height="350px" width="300px" src="http://localhost/ultraludic_ADS/img/1664777756_tumblr_cca4f06484b447c0687f0325af5b38c9_7c751558_1280.gif" alt="¡Anuncio no disponible!"></a></center>
 <?php include('scripts/vistas.php'); ?><br>
 <script src="scripts/clicks.js"></script>
 <p id="couting"></p>
---
+++ scripts/prueba.php
@@ -0,0 +1,43 @@
+<?php
+
+session_start();
+$id_con = $_SESSION['variable'];
+?>
+
+<script type="text/javascript">
+
+alert("<?php echo $id_con; ?>")
+
+const mysql = require('mysql');
+
+const conection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'ultraludic_ads'
+})
+
+conection.connect( (err) =>{
+    if(err) throw err
+    console.log('La conexion funciona')
+})
+
+//consulta
+conection.query('SELECT * FROM campañas', (err, rows)=>{
+    if(err) throw err
+    console.log("Los datos de la tabla son estos: ")
+    console.log(rows)
+    console.log("La cantidad de resultados es: ")
+    console.log(rows.length)
+    const usuarioUno = rows[1]
+    console.log('El usuario se llama '+usuarioUno.nombre_empresa+'y tiene el id: '+usuarioUno.id)
+    console.log("El anuncio ha tenido: "+usuarioUno.clics+" clics")
+})
+
+const valores = window.location.search;
+console.log(valores);
+
+conection.end()
+
+
+</script>
